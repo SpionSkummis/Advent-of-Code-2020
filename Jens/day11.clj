a0b157541b0ba268760251c2a7bdf72aace314a7
Jens Axelsson <j.axelsson@gmail.com>
2020-12-11 17:24:51 +0100

    Dag 11. Lååångsam, men fungerande.

--- Jens/day11.clj
+++ Jens/day11.clj
@@ -1,50 +1,83 @@
-(def input (->> "inputs/day11.txt"
-                slurp
-                clojure.string/split-lines
-                (map #(map identity %))))
+(defn parse-row [y row]
+  (let [coords (map-indexed (fn [x _] [x y]) row)]
+    (as-> coords $
+      (filter #(= \L (nth row (first %))) $)
+      (zipmap $ (repeat \L)))))
 
-(def layout-height (count input))
-(def layout-width (count (first input)))
-
-(defn get-seat [layout [^Integer x ^Integer y]]
-  (nth (nth layout y) x))
+(def input (let [raw (clojure.string/split-lines (slurp "inputs/day11.txt"))]
+             (->> raw
+                  (map #(map identity %))
+                  (map-indexed parse-row)
+                  (apply merge)
+                  (#(assoc % :height (count raw) :width (count (first raw)))))))
 
 (defn occupied? [layout coord]
-  (condp = (get-seat layout coord)
+  (condp = (get layout coord)
     \# true
     false))
 
-(defn adjacent [layout [^Integer x ^Integer y]]
-  (remove #(or (< (first %) 0)
-               (< (second %) 0)
-               (>= (first %) layout-width)
-               (>= (second %) layout-height))
-          [[(dec x) y] [(dec x) (dec y)] [(dec x) (inc y)]
-           [(inc x) y] [(inc x) (dec y)] [(inc x) (inc y)]
-           [x (dec y)] [x (inc y)]]))
+(defn adjacent [layout [x y]]
+  (let [seats [[(dec x) (dec y)] [(dec x) y] [(dec x) (inc y)] [x (dec y)]
+               [x (inc y)] [(inc x) (dec y)] [(inc x) y] [(inc x) (inc y)]]]
+    (map #(get layout %) seats)))
 
 (defn adjacent-occupied [layout coord]
-  (filter #(occupied? layout %) (adjacent layout coord)))
+  (filter #(= \# %) (adjacent layout coord)))
 
-(defn update-seat [layout coord]
+(defn first-visible [layout [x y] [dx dy]]
+  (loop [[cx cy] [(+ x dx) (+ y dy)]]
+    (if (or (< cx 0) (> cx (:width layout))
+            (< cy 0) (> cy (:height layout)))
+      nil
+      (if-let [seat (get layout [cx cy])]
+        seat
+        (recur [(+ cx dx) (+ cy dy)])))))
+
+(defn visible-occupied [layout coord]
+  (let [dirs [[-1 -1] [-1 0] [-1 1] [0 -1] [0 1] [1 -1] [1 0] [1 1]]]
+    (filter #(= \# %) (map #(first-visible layout %1 %2) (repeat coord) dirs))))
+
+(defn update-seat [layout coord threshold spotting-fn]
   (cond
-    (= \. (get-seat layout coord)) \.
     (and (occupied? layout coord)
-         (<= 4 (count (adjacent-occupied layout coord)))) \L
+         (<= threshold (count (spotting-fn layout coord))))
+    [coord \L]
     (and (not (occupied? layout coord))
-         (zero? (count (adjacent-occupied layout coord)))) \#
-    :else (get-seat layout coord)))
+         (zero? (count (spotting-fn layout coord))))
+    [coord \#]
+    :else [coord (get layout coord)]))
 
-(defn step [layout]
-  (map-indexed (fn [y r] (map-indexed (fn [x s] (update-seat layout [x y]))
-                                      r))
-               layout))
+(defn print-layout [layout]
+  (map-indexed (fn [y row]
+                 (apply str
+                        (map-indexed (fn [x _]
+                                       (if-let [s (get layout [x y])]
+                                         s
+                                         \.)) row))) (repeat 10 (repeat 10 []))))
 
-(defn reach-steady-state [layout]
-  (reduce #(if (= %1 %2) (reduced %1) %2) (iterate step layout)))
+(defn step [layout spotting-fn threshold]
+  (do (print-layout layout)
+    (->> (keys layout)
+         (remove keyword?)
+         (mapcat #(update-seat layout % threshold spotting-fn))
+         (apply hash-map)
+         (#(assoc % :width (:width layout) :height (:height layout))))))
+
+(defn reach-steady-state [layout spotting-fn threshold]
+  (reduce #(if (= %1 %2) (reduced %1) %2) (iterate #(step % spotting-fn threshold) layout)))
 
 (defn day11-1 [in]
-  (->> (reach-steady-state in)
+  (->> (reach-steady-state in adjacent-occupied 4)
        (map #(filter (partial = \#) %))
        (map count)
        (reduce +)))
+
+(println (str "Part one: " (day11-1 input)))
+
+(defn day11-2 [in]
+  (->> (reach-steady-state in visible-occupied 5)
+       (map #(filter (partial = \#) %))
+       (map count)
+       (reduce +)))
+
+(println (str "Part two: " (day11-2 input)))
