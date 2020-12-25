e377873b7b81a23868b487ffe055f84924efdda3
EJ <erik_jansson123@hotmail.com>
2020-12-25 22:25:14 +0100

    Dag 7!

--- Erik/day07.py
+++ Erik/day07.py
@@ -1,93 +1,53 @@
-import re, sys
-sys.setrecursionlimit(2500)
+import re
 
-# 111 too low
-
-# ([0-9]+)([ a-z]+)
 raw_data = []
-#with open("Erik/inputs/day07.txt", "r") as f:
-with open("Erik/inputs/day07.txt", "r") as f:
+with open("Erik/inputs/day07.txt","rt") as f:
     for line in f:
         raw_data.append(line.strip())
 
-
-def make_dict(raw):
+def make_contains_dict(in_data):
     main_dict = dict()
-    for line in raw:
+    for line in in_data:
         temp_dict = dict()
         key = line.split(" bags contain ")[0]
         vals = re.findall("([0-9]+)([ a-z]+)", line)
         if vals:
             for match in vals:
-                temp_dict[match[1].rstrip("bags ").lstrip(" ")] = match[0] #Här kan det vara en idé att konvertera till int i framtiden
+                temp_dict[match[1].rstrip("bags").strip(" ")] = int(match[0])
         main_dict[key] = temp_dict
     return main_dict
 
-"""
-def contains_gold(hueg_dict, bag):
-    if "shiny gold" in 
-"""
+def make_appears_in_dict(in_data):
+    main_dict = dict()
+    colours = [x.split(" bags contain")[0] for x in in_data]
+    for line in in_data:
+        for col in colours:
+            if col not in main_dict:
+                main_dict[col] = []
+            if col in line.split("bags contain ")[1]:
+                main_dict[col].append(line.split(" bags contain ")[0])
+    return main_dict
 
-def find_gold_containers(hueg_dict, bag_colour): # Det här är helt feltänkt. Varje steg måste leta vidare. hm.
-    parents = []
-    for key in hueg_dict:
-        if bag_colour in hueg_dict[key]:
-            parents.append(key)
-    for bag in parents:
-        new_parents = find_gold_containers(hueg_dict,bag)
-        for item in new_parents:
-            parents.append(item)
-    return parents
-
-def part_one(hueg_dict, bag_colour):
-    return len(set(find_gold_containers(hueg_dict,bag_colour)))
-
-    """
-    containers = []
-    for key in hueg_dict:
-        if contains_gold(hueg_dict[key]):
-            containers.append(key)
+def get_containers(col, in_dict):
+    containers = set()
+    containers.update(in_dict[col])
+    for cols in list(containers):
+        containers.update(get_containers(cols, in_dict))
     return containers
-    """
 
-
-nice_data = make_dict(raw_data)
-#print(nice_data)
-#print(find_gold_containers(nice_data))
-
-"""
-for key in nice_data:
-    print(key, nice_data[key])
-    if("faded blue" in nice_data[key]):
-        print("Blå?")
-    if(5 in nice_data[key]):
-        print("Fem?")
-"""
-
-print(find_gold_containers(nice_data, "shiny gold"))
-print(len(nice_data))
-print(part_one(nice_data,"shiny gold"))
-
-
-
-"""
-def find_gold_containers(hueg_dict): # Det här är helt feltänkt. Varje steg måste leta vidare. hm.
+def get_contained_bags(col, in_dict):
+    bag_amount = 1
+    if len(in_dict[col]) == 0:
+        return bag_amount
+    for key in in_dict[col]:
+        temp_amount = in_dict[col][key]
+        temp_amount *= get_contained_bags(key,in_dict)
+        bag_amount += temp_amount
+    return bag_amount
     
-    if type(hueg_dict) == str:
-        if hueg_dict == "shiny gold":
-            return True
-        return False
-    for key in hueg_dict:
-        if "shiny gold" in hueg_dict[key]:
-            return True
-        #print(key, type(hueg_dict[key]))
-        #if type(hueg_dict[key]) == dict:
-        return(contains_gold(hueg_dict[key]))
-            
-        
-        if "bright gold" in hueg_dict[key]:
-            return True
-        contains_gold(hueg_dict[key])
-        
-    return False
-"""
+app_dict = make_appears_in_dict(raw_data)
+poss_containers = get_containers("shiny gold", app_dict)
+print(f"{len(poss_containers)} bag colours will sooner or later contain a shiny gold bag")
+
+contains_dict = make_contains_dict(raw_data)
+print(f'The shiny gold bag contains {get_contained_bags("shiny gold", contains_dict)-1} bags') #Svaret blir ett för mycket och jag förstår inte riktigt varför.
